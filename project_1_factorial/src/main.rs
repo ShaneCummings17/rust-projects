317b07fa481d74a0d31c68a85a2c7295260833be
Shane Cummings <shanecummings1999@gmail.com>
2024-05-27 19:44:52 -0400

    Added validation for the factorial problem

--- project_1_factorial/src/main.rs
+++ project_1_factorial/src/main.rs
@@ -3,17 +3,33 @@
 use std::io;
 
 fn main() {
-    println!("Welcome to the factorial calculator! Please enter a number:");
+    println!("Welcome to the factorial calculator!");
 
     // Get user input
-    let mut user_input = String::new();
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Unable to read input from the terminal");
+    let clean_number;
 
-    println!("User input is {}", user_input.trim());
+    loop {
+        let mut user_input = String::new();
+        println!("Please enter a number");
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Unable to read input from the terminal");
 
-    // Check that it's valid input
+        // Check that it's a positive number input
+        let user_input: u32 = match user_input.trim().parse() {
+            Ok(val) => val,
+            Err(_) => {
+                user_input.clear();
+                println!("Your input is not a positive number!");
+                continue
+            }
+        };
+
+        clean_number = user_input;
+        break
+    }
+    
+    println!("User selected {}", clean_number);
 
     // Calculate the factorial
 }
