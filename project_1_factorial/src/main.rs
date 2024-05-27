536d33b6583fed661cf6124d42833d588dab238a
Shane Cummings <shanecummings1999@gmail.com>
2024-05-27 19:29:10 -0400

    Got user input for factorial_project

--- project_1_factorial/src/main.rs
+++ project_1_factorial/src/main.rs
@@ -1,9 +1,17 @@
 // Write a program to find the factorial of a number
 // entered by the user (check for all conditions).
+use std::io;
 
 fn main() {
     println!("Welcome to the factorial calculator! Please enter a number:");
+
     // Get user input
+    let mut user_input = String::new();
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Unable to read input from the terminal");
+
+    println!("User input is {}", user_input.trim());
 
     // Check that it's valid input
 
